746171263a587ce0b23de7bac90442fe8ca1bb40
alexgont1@gmail.com <alexgont1@gmail.com>
2019-02-07 19:57:40 -0600

    add save to file (works for small file, each time open it)

--- app.rb
+++ app.rb
@@ -3,6 +3,14 @@ class Logger
 	def self.say_something
 		puts 'Hi!'
 	end
+
+	def self.log_something wat
+		f = File.open 'log.txt', 'a'
+		f.puts wat
+		f.close
+	end
+
 end
 
 Logger.say_something
+Logger.log_something 'wazzup'
